f5166b74631148f08b5c139f1d2095418de8b165
zchockrl <charlesjparham@gmail.com>
2025-06-28 23:05:44 -0400

    Beginning files

---
+++ FoodCityPOS/Form1.cs
@@ -0,0 +1,17 @@
+namespace FoodCityPOS
+{
+    public partial class Form1 : Form
+    {
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private string loginUserName = "";
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
